6467d82494f4344c52a061580df5ea6c8299103b
sam <koobyte@126.com>
2022-02-16 15:34:42 +0800

    添加中文注释

--- MetaCoin/migrations/1_initial_migration.js
+++ MetaCoin/migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
+// 这是一个部署脚本，用来部署 Migrations 合约，对应 Migrations.sol 文件。
+
 const Migrations = artifacts.require("Migrations");
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+module.exports = function (deployer) {
+    deployer.deploy(Migrations);
 };
--- MetaCoin/migrations/2_deploy_contracts.js
+++ MetaCoin/migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
+// 这是一个部署脚本，用来部署 MetaCoin 合约. (部署脚本的运行是有顺序的，以2开头的脚本通常在以1开头的脚本之后运行)
+
 const ConvertLib = artifacts.require("ConvertLib");
 const MetaCoin = artifacts.require("MetaCoin");
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+module.exports = function (deployer) {
+    deployer.deploy(ConvertLib);
+    deployer.link(ConvertLib, MetaCoin);
+    deployer.deploy(MetaCoin);
 };
