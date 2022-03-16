733deac27cb0ae8ce864ce77f92f24c8bc48b1a0
sam <koobyte@126.com>
2022-03-16 14:25:44 +0800

    call、deletegatecall区别测试

--- solidity/migrations/2_deploy_contracts.js
+++ solidity/migrations/2_deploy_contracts.js
@@ -5,6 +5,7 @@ const caller = artifacts.require("Caller");
 module.exports = function (deployer) {
   deployer.deploy(calledLibrary);
   deployer.deploy(calledContract);
+  // 连接依赖合约
   deployer.link(calledLibrary, caller);
   deployer.link(calledContract, caller);
   deployer.deploy(caller);
