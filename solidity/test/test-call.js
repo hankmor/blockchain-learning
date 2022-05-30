983af73543721f8f1bad19114ca6366f835ab353
huzhou <huzhou.v@gmail.com>
2022-05-30 15:26:00 +0800

    m

---
+++ solidity/test/test-call.js
@@ -0,0 +1,13 @@
+/**
+ * 测试call合约文件
+ */
+
+calledContract = artifacts.require("CalledContract");
+calledLibrary = artifacts.require("CalledLibrary");
+caller = artifacts.require("Caller");
+
+contract('Caller', () => {
+    it('should ', function () {
+
+    });
+});
