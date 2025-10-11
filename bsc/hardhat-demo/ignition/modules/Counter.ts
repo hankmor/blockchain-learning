15825030cad181fb9e1773d015b457285ea63bb7
hank <hankmo.x@gmail.com>
2025-10-11 07:32:36 +0000

    feat: 添加部署方法

--- bsc/hardhat-demo/ignition/modules/Counter.ts
+++ bsc/hardhat-demo/ignition/modules/Counter.ts
@@ -5,6 +5,7 @@ export default buildModule("CounterModule", (m) => {
   const counter = m.contract("Counter");
 
   m.call(counter, "incBy", [5n]);
+  // m.call(counter, "inc");
 
   return { counter };
 });
